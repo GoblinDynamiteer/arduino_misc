b8cf04755077e480f33e6de9d301fb3f8d276d18
Johan Kämpe <kampe.johan@gmail.com>
2016-11-12 16:21:21 +0100

    Minor changes

--- servoPot/servoPot.ino
+++ servoPot/servoPot.ino
@@ -1,9 +1,7 @@
 #include <Servo.h> //För servo-motor funktioner
-
 /*
 Projekt från
 https://www.arduino.cc/en/Tutorial/Knob
-
 
 Potentiometer: b10k (10kOhm)
 	ben 1: gnd
@@ -17,10 +15,8 @@ Samband mellan 0-180 & 0-1023:
 Servo: SG90 9g Micro Servo. Kan rotera 180grader, ~90 åt varje håll
 	
 */
-
-
 Servo servo;	//Skapar servo-object -- likt button-object i button.h
-byte potPin = 0; //Pinne för potentiometer
+byte potPin = 0; //Pinne för potentiometer (ben2)
 byte servoPin = 9;
 float divider = 5.68;
 int grad = 0;
@@ -30,15 +26,14 @@ void setup() {
 	pinMode(11, OUTPUT); //Strömmatning till servo
 	servo.attach(servoPin); //Väljer att servo ska sättas till 'servoPin'
 	Serial.begin(9600);
-
 }
 
-void loop() {
+void loop(){
 	digitalWrite(13, 1);
 	grad = analogRead(potPin) / divider;
 	servo.write(grad);
 	//servo.write(0);
-	Serial.println("Pot:" );
+	Serial.print("Pot:" );
 	Serial.println(analogRead(potPin));
-	delay(10);
+	delay(20);
 }
